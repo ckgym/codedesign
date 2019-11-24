82d0283d416b480c71fb46f3e510b71ac03b717f
chenkun <766251115@qq.com>
2019-11-24 23:52:26 +0800

    测试

--- src/main/java/com/example/design/capation1/BaseOption.java
+++ src/main/java/com/example/design/capation1/BaseOption.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.omg.CORBA.INTERNAL;
 
 /**
- * @ClassName : design1
+ * @ClassName : design
  * @Description : 运算基础类
  * @Author : ChenKun
  * @Date : 2019-11-24 11:44
